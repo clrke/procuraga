6f2586d79bee41b1e8acc9061e7d6f3c2156814e
Paul Aranzamendez <pjinxed.aranzaellej@gmail.com>
2014-11-20 16:56:58 +0800

    Initial homepage

--- procuraga/urls.py
+++ procuraga/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import opportunities.views
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'procuraga.views.home', name='home'),
+    url(r'^$', opportunities.views.home, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
