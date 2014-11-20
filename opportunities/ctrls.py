453ba0c63b6d797f391cbfdfa2fb5798cb2fe8eb
Paul Aranzamendez <pjinxed.aranzaellej@gmail.com>
2014-11-20 17:16:54 +0800

    Replace dict with compact shortcut

--- opportunities/ctrls.py
+++ opportunities/ctrls.py
@@ -1,5 +1,6 @@
 import requests
+from procuraga.shortcuts import compact
 
 def home(request):
 	activities = requests.get('http://student-journal.eu1.frbit.net/activities').json()
-	return {"activities": activities}
+	return compact("activities")
