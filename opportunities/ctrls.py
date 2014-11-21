58c5995426364005e31c7583535ceb2945c0b2b7
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-21 16:26:11 +0800

    Remove unnecessary ctrl from home view

--- opportunities/ctrls.py
+++ opportunities/ctrls.py
@@ -1,10 +1,6 @@
 import requests
 from procuraga.shortcuts import compact
 
-def home(request):
-	activities = requests.get('http://student-journal.eu1.frbit.net/activities').json()
-	return compact("activities")
-
 def activities(request):
 	activities = requests.get('http://student-journal.eu1.frbit.net/activities').json()
 	return compact("activities")
--- opportunities/views.py
+++ opportunities/views.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from . import ctrls
 
-def home(request): 			return render(request, "index.html", ctrls.home(request))
+def home(request): 			return render(request, "index.html")
 def activities(request): 	return JsonResponse(ctrls.activities(request))
