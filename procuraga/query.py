a5c71b3aea07bdaa2a00bf0e6a7eff8d8d58ce1d
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-22 12:10:17 +0800

    Modified query.py

--- procuraga/query.py
+++ procuraga/query.py
@@ -1,5 +1,3 @@
-def query(table_name, p1, p2=None)
-	if (p2==None)
-		return "SELECT * FROM %s LIMIT %s" % table_name p1 p2"
-	else
-		return "SELECT * FROM %s WHERE ROWNUM %s AND ROWNUM %s" % table_name p1 p2"
+def query(table_name, p1)
+		return "SELECT * FROM %s LIMIT %s" % table_name p1"
+
