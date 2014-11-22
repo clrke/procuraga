6364c79f508518888c52f5db7df84c21b0b8eef8
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-22 16:49:53 +0800

    Modified ctrls.py, query.py

--- opportunities/ctrls.py
+++ opportunities/ctrls.py
@@ -1,6 +1,7 @@
 import requests
 from procuraga.shortcuts import compact
 from procuraga import query
+from urllib.parse import urlparse
 
 def awards(request):
 	award = requests.get('http://philgeps.cloudapp.net:5000/api/action/datastore_search_sql?sql='+
@@ -10,5 +11,6 @@ def awards(request):
 
 def bids(request):
 	bids = requests.get('http://philgeps.cloudapp.net:5000/api/action/datastore_search_sql?sql='+
-		query.year('baccd784-45a2-4c0c-82a6-61694cd68c9d', 2009)).json()
+		query.year('539525df-fc9a-4adf-b33d-04747e95f120', '2009')).json()
+	print(query.year('baccd784-45a2-4c0c-82a6-61694cd68c9d', '2%'))
 	return compact("bids")
--- procuraga/query.py
+++ procuraga/query.py
@@ -2,7 +2,8 @@ def get(table_name, start_index, number):
 	return 'SELECT * FROM "%s" LIMIT %s OFFSET %s' % (table_name, number, (number*start_index))
 
 def year(table_name, year, number=-1):
-	if number > 0:
-		return 'SELECT * FROM "%s" WHERE publish_date LIKE %s LIMIT %s' % (table_name, year, number)
-	else:
-		return 'SELECT * FROM "%s" WHERE publish_date LIKE %s' % (table_name, year)
+	#year = str(year)
+	# if number > 0:
+	# 	return 'SELECT * FROM "%s" WHERE publish_date LIKE %s LIMIT %s' % (table_name, year, number)
+	# else:
+	return "SELECT * FROM \"{0}\" WHERE publish_date >= '{1}-01-01' and publish_date <= '{1}-12-31' LIMIT 10".format(table_name, year)
