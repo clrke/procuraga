7a097cd4329fb700a77fc9f8b9e4323f13b303b4
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-22 12:21:32 +0800

    Modified query.py

--- procuraga/query.py
+++ procuraga/query.py
@@ -1,3 +1,3 @@
-def query(table_name, p1)
-		return "SELECT * FROM %s LIMIT %s" % table_name p1"
 
+def get(table_name, start_index, number)
+		return "SELECT * FROM %s LIMIT %s OFFSET %s" % table_name number start_index"
