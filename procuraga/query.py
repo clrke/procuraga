7dd2f08658a14f8d7074afe9121cf7fc4e70cf92
cholo <pjinxed.aranzaellej@gmail.com>
2014-11-22 18:12:10 +0800

    Add price per unit param getter function to query.py

--- procuraga/query.py
+++ procuraga/query.py
@@ -7,3 +7,6 @@ def year(table_name, year, number=-1):
 	# 	return 'SELECT * FROM "%s" WHERE publish_date LIKE %s LIMIT %s' % (table_name, year, number)
 	# else:
 	return "SELECT * FROM \"{0}\" WHERE publish_date >= '{1}-01-01' and publish_date <= '{1}-12-31' LIMIT 10".format(table_name, year)
+def pperUnit(table_name, year,limit):
+	
+	return "SELECT item_name,budget,qty FROM \"{0}\" WHERE  modified_date >= '{1}-01-01' and modified_date <= '{1}-12-31' limit \"{2}\"
