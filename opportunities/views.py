5def49bf854484765d583c43dc5afde9071de584
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-23 11:46:55 +0800

    Fix navbar and urls

--- opportunities/views.py
+++ opportunities/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from . import ctrls
 
-def home(request): 		return render(request, "index.html")
+def home(request): 		return render(request, "index.html", {"active" : "home"})
 def awards(request): 	return JsonResponse(ctrls.awards(request))
 def bids(request): 		return JsonResponse(ctrls.bids(request))
 def pperunit(request):	return JsonResponse(ctrls.pperunit(request))
--- research/ctrls.py
+++ research/ctrls.py
@@ -2,4 +2,5 @@ from procuraga.shortcuts import compact
 
 def home(request):
 	year = request.GET['year'] if 'year' in request.GET else None
-	return compact("year")
+	active = "research"
+	return compact("year", "active")
